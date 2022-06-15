f807283430de9f27165fdf96fcc7b0c7b5bc3f12
fawaaaz111 <48890670+fawaaaz111@users.noreply.github.com>
2022-06-15 11:55:55 +0200

    complete the search section
    finishing up the serach section section with the filters to unable the user to go over variant choices

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
-$(document).ready(function() {
-    // $(".filter_type").click(function() {
-    alert("fwaz");
-    // })
+/*
+    script for scorpion cars website
+    auther : fawaz alhafiz
+    version : 1.0
+*/
+
+ /*  ----------------------------------jquery for showing filters results in the search section ------------------- */
+
+ $(document).ready(function() { // make sure the document is done downloaded
+    // alert("fawaz");
+    $(".filter_type").click(function() {
+        $(this).siblings("ul").toggleClass("active"); // taeget the list with the class "active"
+    });
+
+    $(".filter_item ul li").click(function() {
+        var text = $(this).text(); // get the text from the list item which is pressed
+        $(this).parent().siblings(".filter_type").text(text);// select the "filter type" div to show the text on
+        $(".filter_item ul").removeClass("active");
+    });
 }) ;
